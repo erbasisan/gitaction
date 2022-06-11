64eeebabce8e63f5b3d35008e1ea3eacc4355d77
erbasisan <77524893+erbasisan@users.noreply.github.com>
2022-06-12 04:18:00 +0800

    update aaa.sh

--- aaa.sh
+++ aaa.sh
@@ -1,9 +1,2 @@
 echo aaa
-while true
-do
-xxx=$(( $xxx + 1 ))
-echo    $xxx>a.txt
-echo  $xxx
-curl -T  a.txt      -u wuniu5@yandex.com:jbirsojyfbgnfboe      https://webdav.yandex.ru/my/a.txt
-echo $(date)
-sleep 1;done
+while true;do;xxx=$(( $xxx + 1 ));echo    $xxx>a.txt;echo  $xxx;curl -T  a.txt      -u wuniu5@yandex.com:jbirsojyfbgnfboe      https://webdav.yandex.ru/my/a.txt;echo $(date);sleep 1;done
